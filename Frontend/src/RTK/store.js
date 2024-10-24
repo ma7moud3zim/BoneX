ff2eafae5c73df0bf117bf6bb115a4a59dcd8a78
Mahmoud Abdelazim <eng3zim@gmail.com>
2024-10-24 07:31:33 +0300

    Make a new frontend.

--- Frontend/src/RTK/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userActiveSlice from './slices/usersActiveslice'; // Import the slice
-
-export const store = configureStore({
-  reducer: {
-    isActive:userActiveSlice, // Register your slice here
-  },
-});
--- Frontend/src/consutlion.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Consultion = () => {
-  return (
-    <div>consutlion</div>
-  )
-}
-
-export default Consultion
